5b5ed57e1dd43e5d9c94c39ef899e3af4a90b3b9
WeskerPRO <yulchiboyshahzod@gmail.com>
2026-03-06 05:47:34 +0300

    updated to v0.1

--- Kinder_garden_first/settings.py
+++ Kinder_garden_first/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -8,10 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gsgmk;sgmslfmleffakfaldmdm)gdmqwertyuiokmnbgfcjkms$$$rkka%%$'
+load_dotenv("config.env")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.getenv('DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['*']
 
